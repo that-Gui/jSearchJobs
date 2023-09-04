f3ca612b026b50ab02d219f144336f3e32267d8f
thatGui <thatguifonseca@gmail.com>
2023-09-04 18:56:24 +0100

    fixed the @env import bug

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import * as React from 'react';
 
-import styles from './popularjobcard.style'
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 
-const PopularJobCard = () => {
-  return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
-  )
-}
+import styles from './popularjobcard.style';
+/* import { checkImageURL } from '../../../../utils'; */
 
-export default PopularJobCard
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
+	console.log(item);
+	return (
+		<TouchableOpacity
+			style={styles.container(selectedJob, item)}
+			onPress={() => handleCardPress(item)}
+		>
+			<Text>{item.employer_name}</Text>
+		</TouchableOpacity>
+	);
+};
+
+export default PopularJobCard;
