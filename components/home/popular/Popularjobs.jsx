85140d7eb2ae6759a8dcd65b1bc7df786cabae05
thatGui <thatguifonseca@gmail.com>
2023-09-04 18:14:00 +0100

    trying to fix the env variable import

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,46 +1,62 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { useRouter } from 'expo-router';
-import { SIZES, COLORS } from '../../../constants';
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	FlatList,
+	ActivityIndicator,
+} from 'react-native';
 
 import styles from './popularjobs.style';
-import { FlatList } from 'react-native-gesture-handler';
-import PopularJobCard from '../../../components/common/cards/popular/PopularJobCard';
+import { COLORS, SIZES } from '../../../constants/theme';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 import useFetch from '../../../lib/useFetch';
 
 const Popularjobs = () => {
 	const router = useRouter();
 	const { data, isLoading, error } = useFetch('search', {
-		query: 'JavaScript Developer',
-		page: 1,
-		num_pages: 1,
+		query: 'React developer',
+		num_pages: '1',
 	});
-	console.log(data);
+
+	const [selectedJob, setSelectedJob] = React.useState();
+
+	const handleCardPress = (item) => {
+		router.push(`/job-details/${item.job_id}`);
+		setSelectedJob(item.job_id);
+	};
 
 	return (
 		<View style={styles.container}>
 			<View style={styles.header}>
-				<Text style={styles.headerTitle}>Popular Jobs</Text>
+				<Text style={styles.headerTitle}>Popular jobs</Text>
 				<TouchableOpacity>
-					<Text style={styles.headerBtn}>View All</Text>
+					<Text style={styles.headerBtn}>Show all</Text>
 				</TouchableOpacity>
 			</View>
+
 			<View style={styles.cardsContainer}>
 				{isLoading ? (
 					<ActivityIndicator size='large' color={COLORS.primary} />
 				) : error ? (
-					<Text>{error}</Text>
+					<Text>Something went wrong</Text>
 				) : (
 					<FlatList
 						data={data}
-						renderItem={({ item }) => <PopularJobCard item={item} />}
-						keyExtractor={(item) => item?.job_id}
+						renderItem={({ item }) => (
+							<PopularJobCard
+								item={item}
+								selectedJob={selectedJob}
+								handleCardPress={handleCardPress}
+							/>
+						)}
+						keyExtractor={(item) => item.job_id}
 						contentContainerStyle={{ columnGap: SIZES.medium }}
 						horizontal
 					/>
 				)}
 			</View>
-			{/*  */}
 		</View>
 	);
 };
